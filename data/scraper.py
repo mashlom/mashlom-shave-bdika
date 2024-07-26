16bc26a38b8d7a9af73ebd40f1f192ec8c2de83e
Tal Avinari <talavinari@gmail.com>
2024-07-27 00:00:55 +0300

    update scraper

--- data/scraper.py
+++ data/scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 base_url = "https://www.shavebdika.org.il/ResultsPage2.aspx"
 params = {
@@ -70,7 +71,7 @@ def try_parse(html_text, case_name):
 
 
 def fetch_data_and_parse():
-    for year in range(2006, 2023):
+    for year in range(1910, 2024):
         for male in [True, False]:
             params["Male"] = str(male)
             params["YearBirth"] = year
@@ -81,7 +82,8 @@ def fetch_data_and_parse():
             wellbeing_pair_ids = try_parse(response.text, case_name='wellbeing')
             medical_pair_ids = try_parse(response.text, case_name='medical')
 
-            param_to_pair_ids_mapping[(year, male)] = {
+            param_key = f"{2024-year},{male}"
+            param_to_pair_ids_mapping[param_key] = {
                 'wellbeing_case': wellbeing_pair_ids,
                 'medical_case': medical_pair_ids
             }
@@ -89,7 +91,7 @@ def fetch_data_and_parse():
 
 def print_mappings_and_pairs():
     for params, pair_ids in param_to_pair_ids_mapping.items():
-        print(f"Parameters: YearBirth={params[0]}, Male={params[1]}")
+        print(f"Parameters: {params}")
         print("Wellbeing Case Pair IDs:")
         for pair_id in pair_ids['wellbeing_case']:
             print(f"ID: {pair_id}, Pair: {unique_pairs_wellbeing[pair_id]}")
@@ -106,6 +108,34 @@ def print_mappings_and_pairs():
         print(f"ID: {pair_id}, Pair: {pair}")
 
 
+def transform_pairs(pairs, case_name):
+    transformed_pairs = []
+    for pair_id, (title, description) in pairs.items():
+        transformed_pairs.append({
+            "id": pair_id,
+            "title": title,
+            "description": description
+        })
+    return transformed_pairs
+
+
+def save_to_json_files():
+    # Convert pairs to the desired format
+    formatted_wellbeing_pairs = transform_pairs(unique_pairs_wellbeing, "wellbeing_case")
+    formatted_medical_pairs = transform_pairs(unique_pairs_medical, "medical_case")
+
+    # Save the mappings and pairs to JSON files
+    with open("age_sex_to_data_mapping.json", "w", encoding='utf-8') as outfile:
+        json.dump(param_to_pair_ids_mapping, outfile, ensure_ascii=False, indent=4)
+
+    with open("well_being_pairs.json", "w", encoding='utf-8') as outfile:
+        json.dump(formatted_wellbeing_pairs, outfile, ensure_ascii=False, indent=4)
+
+    with open("medical_advice_pairs.json", "w", encoding='utf-8') as outfile:
+        json.dump(formatted_medical_pairs, outfile, ensure_ascii=False, indent=4)
+
+
 # Execute the functions
 fetch_data_and_parse()
 print_mappings_and_pairs()
+save_to_json_files()
